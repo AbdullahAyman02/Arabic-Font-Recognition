551f6bf83e17a4dde0a64ecb8a5e919026a42f8f
Ali-9-25 <alykhazback@gmail.com>
2024-05-14 15:03:16 +0300

    Changed path of server test to absolute

--- src/scripts/servertest.py
+++ src/scripts/servertest.py
@@ -1,9 +1,11 @@
 import requests
 
 # Open the image file in binary mode
-with open('../data/raw/fonts-dataset/IBM Plex Sans Arabic/5.jpeg', "rb") as file:
+# NOTE: This is an absolute path, change it depending on your computer
+with open('C:/Users/alykh/Downloads/Pattern_Recognition/Project/Arabic-Font-Recognition/src/data/raw/fonts-dataset/IBM Plex Sans Arabic/5.jpeg', "rb") as file:
     # Send a POST request to the server
-    response = requests.post("http://127.0.0.1:8000/predict/", files={"file": file})
+    response = requests.post(
+        "http://localhost:8000/predict/", files={"file": file})
 
 # Print the response
 print(response.json())

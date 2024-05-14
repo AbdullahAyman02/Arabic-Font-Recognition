a9ef06fb18eed2c17b4f4fec9256e5f82c61a52b
Mohamed Heggi <mtheggi@gmail.com>
2024-05-14 16:33:59 +0300

    add_servertest.py

--- src/scripts/servertest.py
+++ src/scripts/servertest.py
@@ -3,7 +3,7 @@ import requests
 # Open the image file in binary mode
 with open('../data/raw/fonts-dataset/IBM Plex Sans Arabic/5.jpeg', "rb") as file:
     # Send a POST request to the server
-    response = requests.post("http://127.0.0.1:8000/predict/", files={"file": file})
+    response = requests.post("http://4.233.223.91/predict/", files={"file": file})
 
 # Print the response
 print(response.json())

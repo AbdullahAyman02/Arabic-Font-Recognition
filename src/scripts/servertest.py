9faff93cb57656c1333f037711b33cf9d7d7b613
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-05-15 00:09:28 +0300

    added training sets paths

--- src/scripts/servertest.py
+++ src/scripts/servertest.py
@@ -4,10 +4,13 @@ import requests
 # NOTE: This is an absolute path, change it depending on your computer
 
 
-with open('../data/raw/fonts-dataset/Marhey/900.jpeg', "rb") as file:
+with open('../data/train/IBM Plex Sans Arabic/0.jpeg', "rb") as file:
+# with open('../data/train/Lemonada/0.jpeg', "rb") as file:
+# with open('../data/train/Marhey/0.jpeg', "rb") as file:
+# with open('../data/train/Scheherazade New/0.jpeg', "rb") as file:
     # Send a POST request to the server
-    # response = requests.post("http://4.233.223.91/predict/", files={"file": file})
-    response = requests.post("http://127.0.0.1:8000/predict/", files={"file": file})
+    response = requests.post("http://4.233.223.91/predict/", files={"file": file})
+    # response = requests.post("http://127.0.0.1:8000/predict/", files={"file": file})
     
 # Print the response
 print(response.json())

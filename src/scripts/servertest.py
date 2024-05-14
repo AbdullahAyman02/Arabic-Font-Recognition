0c6ac60afda6b317ecf5597710f541b77e7ec16f
Mohamed Heggi <mtheggi@gmail.com>
2024-05-14 21:36:10 +0300

    updating server files with new features and new scaler.pkl

--- src/scripts/servertest.py
+++ src/scripts/servertest.py
@@ -4,7 +4,7 @@ import requests
 # NOTE: This is an absolute path, change it depending on your computer
 
 
-with open('../data/raw/fonts-dataset/Marhey/900.jpeg', "rb") as file:
+with open('../data/raw/fonts-dataset/Scheherazade New/98.jpeg', "rb") as file:
     # Send a POST request to the server
     # response = requests.post("http://4.233.223.91/predict/", files={"file": file})
     response = requests.post("http://127.0.0.1:8000/predict/", files={"file": file})

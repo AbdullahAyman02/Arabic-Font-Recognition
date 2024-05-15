c95f46dc8599e9e0132e3480008a8cb8927cca33
Mohamed Heggi <mtheggi@gmail.com>
2024-05-15 10:04:17 +0300

    url changed to the the deployed server

--- src/scripts/servertest.py
+++ src/scripts/servertest.py
@@ -6,8 +6,8 @@ import requests
 
 with open('../data/raw/fonts-dataset/Scheherazade New/98.jpeg', "rb") as file:
     # Send a POST request to the server
-    # response = requests.post("http://4.233.223.91/predict/", files={"file": file})
-    response = requests.post("http://127.0.0.1:8000/predict/", files={"file": file})
+    response = requests.post("http://4.233.223.91/predict/", files={"file": file})
+    # response = requests.post("http://127.0.0.1:8000/predict/", files={"file": file})
     
 # Print the response
 print(response.json())
